ee05b6ccd0fb461b3d4de6994de45afde1766a42
halomademeapc <alex@halomademeapc.com>
2023-03-21 20:04:58 -0400

    get b&w image encoding working

---
+++ src/Contraband.Core/ContrabandEncoder.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using MessagePack;
+
+namespace Contraband.Core;
+
+public class ContrabandEncoder
+{
+    private const int MaxFrameDimension = 4096;
+    private const int MaxFrameArea = MaxFrameDimension * MaxFrameDimension;
+
+    internal async Task<Stream> GenerateFrame<T>(T data, CancellationToken cancellationToken = default)
+    {
+        var serialized = MessagePackSerializer.Serialize(data, cancellationToken: cancellationToken);
+        var header = BitConverter.GetBytes(serialized.Length);
+        var payload = MergeUsingBlockCopy(header, serialized);
+        if (payload.Length * 8 > MaxFrameArea)
+            throw new ArgumentOutOfRangeException(nameof(data), "Value larger than permitted serializable size");
+        using var image = ConvertBytesToFrame(payload);
+        var memoryStream = new MemoryStream();
+        await image.SaveAsPngAsync("test.png", cancellationToken);
+        await image.SaveAsPngAsync(memoryStream, cancellationToken);
+        memoryStream.Seek(0, SeekOrigin.Begin);
+        return memoryStream;
+    }
+
+    internal Image<Rgb24> ConvertBytesToFrame(byte[] payload)
+    {
+        var bits = new BitArray(payload);
+        var frameDimension = CalculateDimension(bits.Length);
+        var image = new Image<Rgb24>(frameDimension, frameDimension);
+        var bitIndex = 0;
+        var white = Color.White;
+        var black = Color.Black;
+        image.ProcessPixelRows(accessor =>
+        {
+            for (var y = 0; y < frameDimension; y++)
+            {
+                var pixelRow = accessor.GetRowSpan(y);
+                for (var x = 0; x < frameDimension; x++)
+                {
+                    pixelRow[x] = bits[bitIndex++] ? white : black;
+
+                    if (bitIndex == bits.Length)
+                        return;
+                }
+            }
+        });
+        return image;
+
+        static int CalculateDimension(int payloadSize)
+        {
+            var squareRoot = Math.Sqrt(payloadSize);
+            return (int)Math.Ceiling(squareRoot);
+        }
+    }
+
+    private static byte[] MergeUsingBlockCopy(byte[] firstArray, byte[] secondArray)
+    {
+        var combinedArray = new byte[firstArray.Length + secondArray.Length];
+        Buffer.BlockCopy(firstArray, 0, combinedArray, 0, firstArray.Length);
+        Buffer.BlockCopy(secondArray, 0, combinedArray, firstArray.Length, secondArray.Length);
+        return combinedArray;
+    }
+}
---
+++ test/Contraband.Core.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using Bogus;
+using FluentAssertions;
+using MessagePack;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.PixelFormats;
+using Xunit.Sdk;
+
+namespace Contraband.Core.Tests.Unit;
+
+public class UnitTest1
+{
+    [Fact]
+    public async Task Generate_CreatesImage_WhenInputIsObject()
+    {
+        var objectToEncode = Generator.Generate();
+        await using var image = await new ContrabandEncoder().GenerateFrame(objectToEncode);
+        image.CanRead.Should().BeTrue();
+    }
+    
+    [Fact]
+    public async Task Generate_CreatesImage_WhenInputIsCollection()
+    {
+        var objectToEncode = Generator.GenerateBetween(2, 5);
+        await using var image = await new ContrabandEncoder().GenerateFrame(objectToEncode);
+        image.CanRead.Should().BeTrue();
+    }
+    
+    [Fact]
+    public async Task Header_ContainsDataLength()
+    {
+        var objectToEncode = Generator.GenerateBetween(1, 50);
+        var dataLength = MessagePackSerializer.Serialize(objectToEncode).Length;
+        await using var content = await new ContrabandEncoder().GenerateFrame(objectToEncode);
+        var bytes = await GetBytes(content);
+        var header = bytes[..4];
+        var headerValue = BitConverter.ToInt32(header);
+        headerValue.Should().Be(dataLength);
+    }
+
+    protected static async Task<byte[]> GetBytes(Stream sourceStream)
+    {
+        var bits = await GetBits(sourceStream).ToListAsync();
+        return bits.Chunk(8).Where(c => c.Length == 8).Select(chunk => ToByte(new BitArray(chunk))).ToArray();
+    }
+
+    protected static async IAsyncEnumerable<bool> GetBits(Stream sourceStream)
+    {
+        var image = await Image.LoadAsync(sourceStream);
+        if (image is not Image<Rgb24> parsedImage)
+            throw new XunitException("Image not recognized");
+
+        foreach (var group in parsedImage.GetPixelMemoryGroup().ToArray())
+            foreach (var pixel in group.ToArray())
+                yield return pixel.R > byte.MaxValue / 2;
+    }
+
+    protected static readonly Faker<MySerializableType> Generator = new Faker<MySerializableType>()
+        .RuleFor(t => t.Potato, faker => faker.Lorem.Sentence())
+        .RuleFor(t => t.Yeet, faker => faker.Random.Bool())
+        .RuleFor(t => t.Okay, faker => faker.Random.Double());
+    
+    public static byte ToByte(BitArray bits)
+    {
+        if (bits.Count != 8)
+        {
+            throw new ArgumentException(nameof(bits));
+        }
+        byte[] bytes = new byte[1];
+        bits.CopyTo(bytes, 0);
+        return bytes[0];
+    }
+
+    [MessagePackObject]
+    public class MySerializableType
+    {
+        [Key(0)] public required string Potato { get; set; }
+
+        [Key(1)] public bool Yeet { get; set; }
+
+        [Key(2)] public double Okay { get; set; }
+    }
+}
